3149cfce62ba5d22385183839cfe15c6de223915
Sinttipon <vdanvik@mail.ru>
2026-04-24 13:49:44 +0300

    chtoto

---
+++ Sequence.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <functional>
+#include "exceptions.hpp"
+
+template <typename T>
+class Sequence
+{
+public:
+    virtual ~Sequence() = default;
+
+    virtual T GetFirst() const = 0;
+    virtual T GetLast() const = 0;
+    virtual T Get(int index) const = 0;
+    virtual int GetLength() const = 0;
+
+    virtual Sequence<T> *GetSubsequence(int startIndex, int endIndex) const = 0;
+
+    virtual Sequence<T> *Append(const T &item) const = 0;
+    virtual Sequence<T> *Prepend(const T &item) const = 0;
+    virtual Sequence<T> *InsertAt(const T &item, int index) const = 0;
+    virtual Sequence<T> *Concat(const Sequence<T> *other) const = 0;
+
+    template <typename TOut>
+    virtual Sequence<TOut> *Map(std::function<TOut(const T &)> func) const = 0;
+
+    virtual Sequence<T> *Where(std::function<bool(const T &)> predicate) const = 0;
+
+    template <typename TAcc>
+    virtual TAcc Reduce(std::function<TAcc(const TAcc &, const T &)> func, const TAcc &init) const = 0;
+
+protected:
+    virtual Sequence<T> *Instance() const = 0;
+    virtual Sequence<T> *Clone() const = 0;
+};
